35f9b3fddf6eda97d8ea4e1ee78bb6f85204e7ef
nsc-de <64435955+nsc-de@users.noreply.github.com>
2024-03-31 17:02:39 +0000

    [Automated] Format using prettier

--- packages/timeout-promise-wrapper/lib/index.d.ts
+++ packages/timeout-promise-wrapper/lib/index.d.ts
@@ -1,7 +1,7 @@
 export type Timeout = Promise<void> & {
-    stop(): void;
-    resolve(arg?: unknown): void;
-    reject(arg?: unknown): void;
+  stop(): void;
+  resolve(arg?: unknown): void;
+  reject(arg?: unknown): void;
 };
 export default function timeout(timeout: number): Timeout;
 export { timeout };
